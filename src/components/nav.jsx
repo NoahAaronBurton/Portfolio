f48c92f6d74418e618aea4a1ada1e1f8dfa1262e
Noah Burton <nburton115@gmail.com>
2023-11-04 14:17:57 -0600

    set name heaer to 75%, nav icon

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,13 +1,35 @@
-function Nav() {
+//! remember to put leading slashes on the imports/
+import menuIcon from '/src/assets/menu-icon.png'
 
+function Nav() {
+    
+    const buttonStyle = {
+       backgroundImage: `url(${menuIcon})`, // Set the PNG image as the background
+        backgroundSize: '40px 40px', // Adjust the size as needed
+        backgroundRepeat: 'no-repeat',
+        width: '40px', 
+        height: '40px', 
+        border: 'none',
+        cursor: 'pointer', // Add a pointer cursor to indicate it's clickable
+      };
 
     return (
         <nav className="navbar navbar-dark bg-transparent">
             <div className="container">
                 {/* <a className="navbar-brand" href="#">logo</a> */}
-                <button id="menu-button" className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+                <a 
+                    style={buttonStyle}                           
+                    id="menu-button" 
+                    className="navbar-toggler" 
+                    type="button" 
+                    data-bs-toggle="collapse" 
+                    data-bs-target="#navbarNav" 
+                    aria-controls="navbarNav" 
+                    aria-expanded="false" 
+                    aria-label="Toggle navigation"
+                    >
+                
+                </a>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
