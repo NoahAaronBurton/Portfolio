6bf23fd760a73dc8cf9ea4b8a44b014ed8b1a485
Noah Burton <nburton115@gmail.com>
2023-11-05 13:40:02 -0700

    nav bar sizing, rm  breakpoints for cols

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
-// import { useState } from 'react';
-import Header from './components/header'
-import Nav from './components/nav'
-import './App.css'
+
+import Nav from './components/nav';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <div className='container'>
-        <div className='position-fixed top-0 start-0'>
-          <div className='row' id='header-row'>
-            <div className='col-sm-2'><Nav /></div>
-            <div className='col-sm-10'><Header /></div>
-         </div>
-
-        </div>
-        <div className='row' id='about-row'>
-          <div className='col-sm-2'>  </div>
-          <div className='col-sm-10'><p> <em>Breakpoints</em>   are specific screen dimensions at which the layout of your website can change to provide a better user experience on different devices</p></div>
+    <div className="container-fluid">
+      {/* Header Row */}
+      <div className='position-fixed top-0 start-0 '>
+        <div className="row align-items-start" id="header-row">
+          <div className="col-2">
+          <Nav />
+          </div>
+          <div className="col-10">
+            <div id="header">
+              <img
+                src="src/assets/name-header.png"
+                alt="Noah-Aaron-Burton"
+                id="name-header"
+                className="img-fluid"
+              />
+            </div>
+          </div>
         </div>
       </div>
-  </>
-);}
+      {/* About Me Row */}
+      <div className="row align-items-start" id="about-row">
+        <div className="col-2"></div>
+        <div className="col-10">
+          
+        </div>
+      </div>
+    </div>
+  );
+}
 
-export default App
+export default App;
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -5,41 +5,48 @@ function Nav() {
     
     const buttonStyle = {
        backgroundImage: `url(${menuIcon})`, // Set the PNG image as the background
-        backgroundSize: '40px 40px', // Adjust the size as needed
+        backgroundSize: '60px 60px', //? make larger
         backgroundRepeat: 'no-repeat',
-        width: '40px', 
-        height: '40px', 
+        width: '60px', 
+        height: '60px', //? make larger 
         border: 'none',
         cursor: 'pointer', // Add a pointer cursor to indicate it's clickable
+
       };
 
-    return (
-        <nav className="navbar navbar-dark bg-transparent">
-            <div className="container">
-                {/* <a className="navbar-brand" href="#">logo</a> */}
-                <a 
-                    style={buttonStyle}                           
-                    id="menu-button" 
-                    className="navbar-toggler" 
-                    type="button" 
-                    data-bs-toggle="collapse" 
-                    data-bs-target="#navbarNav" 
-                    aria-controls="navbarNav" 
-                    aria-expanded="false" 
-                    aria-label="Toggle navigation"
-                    >
-                
+      return (
+        <div id="nav-container"> 
+          <a
+            style={buttonStyle}
+            id="menu-button"
+            className="navbar-toggler" // Remove this class
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          ></a>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Home
                 </a>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Home</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    )
-}
+              </li>
+              <li className='nav-item'>
+                <a href="/" className='nav-link'>About Me</a>
+              </li>
+              <li className='nav-item'>
+                <a href="/" className='nav-link'>Portfolio</a>
+              </li>
+              <li className='nav-item'>
+                <a href="/" className='nav-link'>Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      );
+    }
 
 export default Nav;
