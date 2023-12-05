02756c3b7c833a4e89aeeb3410249f1a1130ec10
Noah Burton <nburton115@gmail.com>
2023-12-05 14:43:32 -0700

    contact: github link href fixed

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
         <div className='contact-container'>
             <p>Feel free to reach out with any collaboration inquires or questions about my projects:</p>
             <h4>nburton115@gmail.com</h4>
-            <a href="https://github.com/noahburton">
+            <a href="https://github.com/NoahAaronBurton">
                 <img src="https://img.shields.io/badge/GitHub-NoahAaronBurton-black" alt="GitHub Badge" />
             </a>
         </div>
